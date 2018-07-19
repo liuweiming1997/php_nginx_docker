991e4c898de391fbfc5de68fabff443f173b09c6
vimi <vimiming@gmail.com>
2018-07-19 10:47:05 +0800

    foreach two-dimension

---
+++ docker/html/another/test.php
@@ -0,0 +1,5 @@
+<?php
+define("FW", "vimi2");
+defined("FW") || exit("double define");
+echo FW;
+?>
--- docker/html/index.php
+++ docker/html/index.php
@@ -1,2 +1,12 @@
 <?php
-phpinfo();
+$age = array(
+	"vimi" => array(1, 2, 3, 4),
+	"jake" => array(1, 2, 3, "vimi"));
+
+foreach($age as $i_value) {
+	foreach ($i_value as $j_key => $j_value) {
+		echo "$j_key" . ":" . "$j_value    ";
+	}
+	echo "\n";
+}
+
