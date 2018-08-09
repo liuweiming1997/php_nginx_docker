0e43a5f79a92905f8b628df0696f01aeb50ebfec
vimi <vimiming@gmail.com>
2018-08-09 14:57:22 +0800

    change time

--- docker/html/index.php
+++ docker/html/index.php
@@ -1,9 +1,11 @@
-<?php
-header("Content-type:application/text");
-
-// 文件将被称为 downloaded.pdf
-header("Content-Disposition:attachment;filename='index.php'");
-
-// PDF 源在 original.pdf 中
-readfile("index.php");
-?>
+<?php 
+	$one = 1;
+	$date = '2018-8-9 14:20';
+	echo(strtotime($date));
+	echo("\n");
+	$date = date('Y-m-d 00:00:00', strtotime($date));
+	print($date);
+	print("\n");
+	$end_date = strtotime('now') - strtotime('now') % 60; // 获取分钟整数
+	echo($end_date);
+ ?>
